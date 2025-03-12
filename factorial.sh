d08707892c1ddd1d3c6cdbf9187ade964827a264
Hamdan Mohammed <hamdanmohammed01234@gmail.com>
2025-03-13 02:46:10 +0530

    sECOND2

--- factorial.sh
+++ factorial.sh
@@ -1,8 +1,10 @@
 echo "Enter a number : "
 read n
+
 i=1
 fact=1
-while [ $i -le $n ]
+while [ $i -le $n ]  #because factorial of zero not needed
+ 
 do
 fact=`expr $fact \* $i`
 i=`expr $i + 1`
---
+++ system_info.sh
@@ -0,0 +1,13 @@
+now=$(date +"%d/%m/%y") #fetches date in DD/MM/YY format
+echo "current date: $now"
+now=$(date +%T) # fetches time (T)
+echo "current time: $now"
+
+echo "now `who|wc -l` users are logged in to the system"  #"who" logged-in user and wc-l counts it, wc stands for word count and -l means for line
+echo "current terminal : `tty` "
+echo "login date and time:"
+echo `who` # prints details of logged-in user like ip-address, username etc.
+
+
+
+#may not work properly in WSL or Subsystems.
